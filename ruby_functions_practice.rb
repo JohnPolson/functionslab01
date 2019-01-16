065b3c55d2ffbcb36ba97c7bbcae95d0952b7dbd
JohnPolson <JP@JPs-MacBook-Pro.local>
2019-01-16 00:18:08 +0000

    MVP completion

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -1,3 +1,93 @@
 def return_10
   return 10
 end
+
+def add(firstnumber, secondnumber)
+  return firstnumber + secondnumber
+end
+
+def subtract(firstnumber, secondnumber)
+  return firstnumber - secondnumber
+end
+
+def multiply(firstnumber, secondnumber)
+  return firstnumber * secondnumber
+end
+
+def divide(firstnumber, secondnumber)
+  return firstnumber / secondnumber
+end
+
+def length_of_string(string)
+  return string.length
+end
+
+def join_string(string_1, string_2)
+  return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+  return string_1.to_i + string_2.to_i
+end
+
+def number_to_full_month_name(long)
+  return case long
+  when 1
+    "January"
+  when 2
+    "February"
+  when 3
+    "March"
+  when 4
+    "April"
+  when 5
+    "May"
+  when 6
+    "June"
+  when 7
+    "July"
+  when 8
+    "August"
+  when 9
+    "September"
+  when 10
+    "October"
+  when 11
+    "November"
+  when 12
+    "December"
+  else
+    "Not a valid month"
+  end
+end
+
+  def number_to_short_month_name(short)
+    return case short
+    when 1
+      "Jan"
+    when 2
+      "Feb"
+    when 3
+      "Mar"
+    when 4
+      "Apr"
+    when 5
+      "May"
+    when 6
+      "Jun"
+    when 7
+      "Jul"
+    when 8
+      "Aug"
+    when 9
+      "Sep"
+    when 10
+      "Oct"
+    when 11
+      "Nov"
+    when 12
+      "Dec"
+    else
+      "Not a valid month"
+    end
+  end
